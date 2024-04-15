3d11e8006652b5532cc1ee858d38be4a8067e887
harperers <max@pan-ami.com>
2024-04-14 20:09:17 -0700

    updated registration-form, lab day 3

--- registration-form/routes/index.js
+++ registration-form/routes/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const auth = require('http-auth');
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 const Registration = mongoose.model('Registration');
@@ -33,12 +34,22 @@ router.post('/',
         check('email')
         .isLength({ min: 1 })
         .withMessage('Please enter an email'),
+        check('username')
+        .isLength({ min: 1 })
+        .withMessage('Please enter a username'),
+        check('password')
+        .isLength({ min: 1 })
+        .withMessage('Please enter a password'),
     ],
-    (req, res) => {
+    async (req, res) => {
         //console.log(req.body);
         const errors = validationResult(req);
         if (errors.isEmpty()) {
           const registration = new Registration(req.body);
+          // generate salt to hash password
+          const salt = await bcrypt.genSalt(10);
+          // set user password to hashed password
+          registration.password = await bcrypt.hash(registration.password, salt);
           registration.save()
             .then(() => {res.send('Thank you for your registration!');})
             .catch((err) => {
